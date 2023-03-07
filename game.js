70624a48a8a645affeb3345568b09897295c0dea
Isaiah Richardson <capcinematic@gmail.com>
2023-03-07 14:30:58 -0700

    formatting and alphebetical css

--- game.js
+++ game.js
@@ -1,89 +1,90 @@
-var gameDisplay = document.querySelector(".game-display")
-var winCounterBox = document.querySelector(".win-counter")
-var winCounterBox2 = document.querySelector(".win-counter2")
-
-var quotes = [
-  "Check Your Vibes",
-  "There is no spoon", 
-  "Time is not real",
-  "Wow!",
-  "Do Your Thing!",
-  "Tic-Tac-No!"]
-
+var gameDisplay = document.querySelector(".game-display");
+var winCounterBox = document.querySelector(".win-counter");
+var winCounterBox2 = document.querySelector(".win-counter2");
+var titleBox = document.querySelector(".game-title");
 
 class Game {
-  constructor(player1, player2){
-    this.winningCombos = [[1,2,3],[4,5,6],[7,8,9],[1,4,7],[2,5,8],[3,6,9],[3,5,7],[1,5,9]]
-    this.player1 = player1
-    this.player2 = player2
-    this.turn = player1
-    this.moves = 0
-    this.winner = undefined
+  constructor(player1, player2) {
+    this.winningCombos = [
+      [1, 2, 3],
+      [4, 5, 6],
+      [7, 8, 9],
+      [1, 4, 7],
+      [2, 5, 8],
+      [3, 6, 9],
+      [3, 5, 7],
+      [1, 5, 9],
+    ];
+    this.player1 = player1;
+    this.player2 = player2;
+    this.turn = player1;
+    this.moves = 0;
+    this.winner = undefined;
   }
-  
-  switchTurns(){
-    if(this.turn === this.player1){
-     this.turn = this.player2
-    } else if (this.turn === this.player2){
-      this.turn = this.player1
+
+  switchTurns() {
+    if (this.turn === this.player1) {
+      this.turn = this.player2;
+    } else if (this.turn === this.player2) {
+      this.turn = this.player1;
+    }
+    gameDisplay.innerText = `It's ${this.turn.id}'s Turn!`;
+  }
+
+  buttonSwitch(disabled) {
+    for (var i = 0; i < tokenBoxes.length; i++) {
+      tokenBoxes[i].disabled = disabled;
     }
   }
 
-  buttonSwitch(disabled){
-    for (var i = 0; i < tokenBoxes.length; i++){
-      tokenBoxes[i].disabled = disabled
-    }
-  }
-
-  checkForWins(){ 
-    this.moves ++
-    gameDisplay.innerText = quotes[getRandomIndex(quotes)]
-    for (var i = 0; i < this.winningCombos.length; i++){
-      var combos = this.winningCombos[i]
-      if(this.player1.selectedTiles.includes(combos[0]) && this.player1.selectedTiles.includes(combos[1]) && this.player1.selectedTiles.includes(combos[2])){
-        this.winner = this.player1
-        this.player1.increaseWins()
-        gameDisplay.innerHTML = `${this.player1.id} Wins!`
-        winCounterBox.innerHTML = this.player1.wins
-        // this.resetBoard()
-        this.buttonSwitch(true)
-      } else if (this.player2.selectedTiles.includes(combos[0]) && this.player2.selectedTiles.includes(combos[1]) && this.player2.selectedTiles.includes(combos[2])) {
-        this.winner = this.player2
-        this.player2.increaseWins()
-        gameDisplay.innerHTML = `${this.player2.id} Wins!`
-        winCounterBox2.innerHTML = this.player2.wins
-        this.buttonSwitch(true)
-      } 
-    }
-    
-  }
-// May have to re-order what happens first
-  checkForDraw(){
-    if(this.moves === 9 && this.winner === undefined){
-      console.log("it was a draw")
-      this.winner = "Draw"
-      gameDisplay.innerHTML = `It's a draw!`
-      this.buttonSwitch(true)
-    }
-  }
-
-  // small function that enables, disables buttons
-  resetBoard(){
-      if(this.winner === this.player1 || this.winner === this.player2 || this.winner === "Draw"){
-        this.moves = 0
-        this.player1.selectedTiles = []
-        this.player2.selectedTiles = []
-        // this.player1.wins = 
-        // this.player2.wins = 0
-        this.winner = undefined
-        console.log("Reset Board")
+  checkForWins() {
+    this.moves++;
+    for (var i = 0; i < this.winningCombos.length; i++) {
+      var combos = this.winningCombos[i];
+      if (
+        this.player1.selectedTiles.includes(combos[0]) &&
+        this.player1.selectedTiles.includes(combos[1]) &&
+        this.player1.selectedTiles.includes(combos[2])
+      ) {
+        this.winner = this.player1;
+        this.player1.increaseWins();
+        titleBox.innerHTML = `🤍${this.player1.id} Wins!🤍`;
+        winCounterBox.innerHTML = this.player1.wins;
+        this.buttonSwitch(true);
+      } else if (
+        this.player2.selectedTiles.includes(combos[0]) &&
+        this.player2.selectedTiles.includes(combos[1]) &&
+        this.player2.selectedTiles.includes(combos[2])
+      ) {
+        this.winner = this.player2;
+        this.player2.increaseWins();
+        titleBox.innerHTML = `🖤${this.player2.id} Wins!🖤`;
+        winCounterBox2.innerHTML = this.player2.wins;
+        this.buttonSwitch(true);
       }
-      
     }
-  
-}
+  }
 
-function getRandomIndex(array) {
-  return Math.floor(Math.random() * array.length);
-}
+  checkForDraw() {
+    if (this.moves === 9 && this.winner === undefined) {
+      console.log("it was a draw");
+      this.winner = "Draw";
+      titleBox.innerHTML = `🖤It's a Draw!🤍`;
+      this.buttonSwitch(true);
+    }
+  }
 
+  resetBoard() {
+    if (
+      this.winner === this.player1 ||
+      this.winner === this.player2 ||
+      this.winner === "Draw"
+    ) {
+      this.moves = 0;
+      this.player1.selectedTiles = [];
+      this.player2.selectedTiles = [];
+      this.winner = undefined;
+      console.log("Reset Board");
+    }
+  }
+}
--- main.js
+++ main.js
@@ -1,55 +1,44 @@
-// file that contains all DOM related JavaScript
-var yin = new Player("Player1", "🤍", "Yin")
-var yang = new Player("Player2", "🖤", "Yang")
-var newGame = new Game(yin, yang)
-var gameGrid = document.querySelector(".grid-container")
-var asideClass = document.querySelector(".aside")
-var player1 = document.querySelector("#player-1")
-var player2 = document.querySelector("#player-2")
-var actionSection = document.querySelector(".action-section")
-var tokenBoxes = document.querySelectorAll(".token-box")
-// var winCounterBoxDisplay = document.querySelector(".win-counter")
+var yin = new Player("Player1", "🤍", "Yin");
+var yang = new Player("Player2", "🖤", "Yang");
+var newGame = new Game(yin, yang);
+var gameGrid = document.querySelector(".grid-container");
+var asideClass = document.querySelector(".aside");
+var player1 = document.querySelector("#player-1");
+var player2 = document.querySelector("#player-2");
+var actionSection = document.querySelector(".action-section");
+var tokenBoxes = document.querySelectorAll(".token-box");
 
-// gameGrid.addEventListener("click", takeTurn)
-for (var i = 0; i < tokenBoxes.length; i++){
-  tokenBoxes[i].addEventListener("click", takeTurn)
+for (var i = 0; i < tokenBoxes.length; i++) {
+  tokenBoxes[i].addEventListener("click", takeTurn);
 }
 
-function takeTurn(event){
-  event.target.disabled = true
-  console.log(newGame.turn)
-  placeToken(event)
-  newGame.turn.addTile(event.target.id)
-  newGame.checkForWins()
-  newGame.checkForDraw()
-  newGame.switchTurns()
-  if(newGame.winner){
-    setTimeout(clearBoard, 3000)
-    console.log("reset condition")
-    console.log(yin, yang)
+function takeTurn(event) {
+  event.target.disabled = true;
+  console.log(newGame.turn);
+  placeToken(event);
+  newGame.turn.addTile(event.target.id);
+  newGame.checkForWins();
+  newGame.checkForDraw();
+  newGame.switchTurns();
+  if (newGame.winner) {
+    setTimeout(clearBoard, 3000);
   }
-  
 }
-  
 
-function placeToken(event){
-  // event.target.disabled = true
-  if(newGame.turn === yin){
-    event.target.innerText= yin.token
+function placeToken(event) {
+  if (newGame.turn === yin) {
+    event.target.innerText = yin.token;
   } else {
-    event.target.innerText= yang.token
+    event.target.innerText = yang.token;
   }
 }
 
-
-function clearBoard(){
-  for (var i = 0; i < tokenBoxes.length; i++){
-    tokenBoxes[i].innerHTML = ""
-    tokenBoxes[i].disabled = false
+function clearBoard() {
+  for (var i = 0; i < tokenBoxes.length; i++) {
+    tokenBoxes[i].innerHTML = "";
+    tokenBoxes[i].disabled = false;
   }
-  newGame.resetBoard()
-  gameDisplay.innerHTML = "Focus On balance"
-  console.log("clear board")
+  newGame.resetBoard();
+  gameDisplay.innerHTML = "Focus On balance";
+  titleBox.innerHTML = "Yin Yang Yo!";
 }
- 
-
